3b4448256ad0424647300a79bc632bbf277b7529
Graham Chapman <bgchpmn@gmail.com>
2014-01-02 17:43:46 -0600

    About to refactor functional_tests.py to reduce duplication

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
 def home_page(request):
     """docstring for home_page"""
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '')
+    })
