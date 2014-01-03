7a43d41cc9be4f6e2613c864c67dcb515aac8ce9
Graham Chapman <bgchpmn@gmail.com>
2014-01-03 08:45:54 -0600

    Move functional_tests to functional_tests app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+# import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         """docstring for setUp"""
@@ -24,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -67,5 +68,5 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep.
         
-if __name__ == '__main__':
-        unittest.main(warnings='ignore')
+# if __name__ == '__main__':
+#         unittest.main(warnings='ignore')
